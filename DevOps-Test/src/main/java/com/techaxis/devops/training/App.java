2d5dc97ddac69c1f43a4021af9dd76a19ac59787
Sanjay Subedi <ssu@logpoint.com>
2021-12-03 07:26:25 +0545

    added again

--- DevOps-Test/src/main/java/com/techaxis/devops/training/App.java
+++ DevOps-Test/src/main/java/com/techaxis/devops/training/App.java
@@ -2,12 +2,11 @@ package com.techaxis.devops.training;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+        System.out.println("Added line..");
     }
 }
